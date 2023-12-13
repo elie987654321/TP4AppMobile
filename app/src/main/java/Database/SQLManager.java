58765cd4dff32b081414d597ba76b087278530da
Charlis70 <126210115+Charlis70@users.noreply.github.com>
2023-12-13 08:13:26 -0500

    Amelioration du SQLManager

--- app/src/main/java/Database/SQLManager.java
+++ app/src/main/java/Database/SQLManager.java
@@ -2,9 +2,14 @@ package Database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+
+import java.util.List;
 
 public class SQLManager extends OrmLiteSqliteOpenHelper {
 
@@ -33,9 +38,86 @@ public class SQLManager extends OrmLiteSqliteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion)
-    {
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource,
+                          int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable( connectionSource, Pizza.class, true );
+            TableUtils.dropTable( connectionSource, Client.class, true );
+            TableUtils.dropTable( connectionSource, Commande.class, true );
+            onCreate( database, connectionSource);
+            Log.i( "DATABASE", "onUpgrade invoked" );
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Can't upgrade Database", exception );
+        }
+    }
 
+    public void insertClient( Client client ) {
+        try {
+            //Dao<T, ?> dao = DaoManager.createDao(getConnectionSource(), clazz);
+            Dao<Client, Integer> dao = getDao( Client.class );
+            dao.create( client );
+            Log.i( "DATABASE", "Insertion client" );
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible d'insérer le client dans la base de données", exception );
+        }
+    }
+
+    public List<Client> readClients() {
+        try {
+            Dao<Client, Integer> dao = getDao( Client.class );
+            List<Client> clients = dao.queryForAll();
+            Log.i( "DATABASE", "Lecture des clients" );
+            return clients;
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible de lire les clients la base de données", exception );
+            return null;
+        }
+    }
+
+    public void insertCommande( Commande commande ) {
+        try {
+            //Dao<T, ?> dao = DaoManager.createDao(getConnectionSource(), clazz);
+            Dao<Commande, Integer> dao = getDao( Commande.class );
+            dao.create( commande );
+            Log.i( "DATABASE", "Insertion commande" );
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible d'insérer la commande dans la base de données", exception );
+        }
+    }
+
+    public List<Commande> readCommandes() {
+        try {
+            Dao<Commande, Integer> dao = getDao( Commande.class );
+            List<Commande> commandes = dao.queryForAll();
+            Log.i( "DATABASE", "Lecture des commandes" );
+            return commandes;
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible de lire les commandes la base de données", exception );
+            return null;
+        }
+    }
+
+    public void insertPizza( Pizza pizza ) {
+        try {
+            //Dao<T, ?> dao = DaoManager.createDao(getConnectionSource(), clazz);
+            Dao<Pizza, Integer> dao = getDao( Pizza.class );
+            dao.create( pizza );
+            Log.i( "DATABASE", "Insertion pizza" );
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible d'insérer la pizza dans la base de données", exception );
+        }
+    }
+
+    public List<Pizza> readPizzas() {
+        try {
+            Dao<Pizza, Integer> dao = getDao( Pizza.class );
+            List<Pizza> pizzas = dao.queryForAll();
+            Log.i( "DATABASE", "Lecture des pizzas" );
+            return pizzas;
+        } catch( Exception exception ) {
+            Log.e( "DATABASE", "Impossible de lire les clients la base de données", exception );
+            return null;
+        }
     }
 
 }
